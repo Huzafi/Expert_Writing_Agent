63032d59146dbfaf89e97ebcc2e9b01d1c48373a
Muhammad Huzaifa <huzaifakhan87875647@gmail.com>
2025-10-01 21:54:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ external_client = AsyncOpenAI(
 
 # Create model object
 model = OpenAIChatCompletionsModel(
-    model="gemini-1.5-flash",
+    model="gemini-2.0-flash",
     openai_client=external_client
 )
 
